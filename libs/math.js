7052c5c75899dd51615a29af77f93738b3277c9c
Miguel del Mazo <migueldelmazo@gmail.com>
2017-11-21 17:18:15 +0100

    Added math library

--- libs/math.js
+++ libs/math.js
@@ -1,5 +1,5 @@
 const microTasks = require('../src')
 
-microTasks.methodRegister('utils.add', (a = 0, b = 1) => a + b)
+microTasks.methodRegister('math.add', (a = 0, b = 1) => a + b)
 
-microTasks.methodRegister('utils.substract', (a = 0, b = 1) => a - b)
+microTasks.methodRegister('math.substract', (a = 0, b = 1) => a - b)
