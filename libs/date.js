5cec8d2872aa96910e56a7f0c8d974b3163623aa
Miguel del Mazo <migueldelmazo@gmail.com>
2017-12-19 16:27:24 +0100

    Added date.getDate and date.getMidNight methods

--- libs/date.js
+++ libs/date.js
@@ -6,6 +6,19 @@ const microTasks = require('../src')
 
 /**
  * @function
+ * @returns {boolean} Returns current date.
+ * @name 'date.getDate'
+ * @example
+ * microTasks.taskRun([{
+ *   method: 'date.getDate',
+ *   resultPath: 'now'
+ * }])
+ * // payload.now = Tue Dec 19 2017 16:24:44 GMT+0100 (CET)
+ */
+microTasks.methodRegister('date.getDate', () => new Date())
+
+/**
+ * @function
  * @returns {boolean} Returns now date timestamp.
  * @name 'date.getNow'
  * @example
@@ -13,7 +26,7 @@ const microTasks = require('../src')
  *   method: 'date.getNow',
  *   resultPath: 'now'
  * }])
- * // payload.isLater = 1511284457000
+ * // payload.now = 1511284457000
  */
 microTasks.methodRegister('date.getNow', () => Date.now())
 
@@ -62,6 +75,23 @@ microTasks.methodRegister('date.isBeforeThanNow', (value) => value < Date.now())
  */
 microTasks.methodRegister('date.toDate', (date) => {
   return new Date(date)
+})
+
+/**
+ * Returns a midnight of a date.
+ * @function
+ * @name 'date.toMidnight'
+ * @param {*} date date
+ * @example
+ * microTasks.taskRun([{
+ *   method: 'date.toMidnight',
+ *   params: 'Tue Nov 21 2017 18:14:17 GMT+0100',
+ *   resultPath: 'date'
+ * }])
+ * // payload.date = 1511222400000
+ */
+microTasks.methodRegister('date.toMidnight', (date) => {
+  return new Date(date).setUTCHours(0, 0, 0, 0)
 })
 
 /**
